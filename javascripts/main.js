e41378eba2e5e48d6f08aadee127e531f409eb25
joeslaughter <joseph.r.slaughter@gmail.com>
2015-01-23 16:01:18 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+
+<script type="text/javascript" src="//cdn.sublimevideo.net/js/lpez3wqv.js">
+
+<video id="IjeXN4yxAg4" class="sublime" width="640" height="360" data-uid="IjeXN4yxAg4" data-youtube-id="IjeXN4yxAg4" preload="none">
+
+</video>
+</script>
