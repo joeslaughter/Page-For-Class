9a63fc14ff5f0c49afcbfcfb3ba2361f9a1432cd
joeslaughter <joseph.r.slaughter@gmail.com>
2015-01-23 16:13:27 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,6 @@
 
-<script type="text/javascript" src="//cdn.sublimevideo.net/js/lpez3wqv.js">
+
 
 <video id="IjeXN4yxAg4" class="sublime" width="640" height="360" data-uid="IjeXN4yxAg4" data-youtube-id="IjeXN4yxAg4" preload="none">
 
 </video>
-</script>
